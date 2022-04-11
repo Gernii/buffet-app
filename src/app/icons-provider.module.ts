f3b4b37056ea757feeb4687a768c0ec4cb71a6cb
Le Ngo Duc Anh <54741529+Gernii@users.noreply.github.com>
2022-04-11 17:56:46 +0700

    style: :lipstick: add more icon

--- src/app/icons-provider.module.ts
+++ src/app/icons-provider.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { NZ_ICONS, NzIconModule } from 'ng-zorro-antd/icon';
 
 import {
+  CheckOutline,
   EditOutline,
   MoneyCollectOutline,
   UserOutline,
 } from '@ant-design/icons-angular/icons';
 
-const icons = [UserOutline, MoneyCollectOutline, EditOutline];
+const icons = [UserOutline, MoneyCollectOutline, EditOutline, CheckOutline];
 
 @NgModule({
   imports: [NzIconModule],
